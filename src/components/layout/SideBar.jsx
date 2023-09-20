de2d3788e82e9d160230eadc906838622e52e4cb
Negar Lashgari <negar.lashgari@gmail.com>
2023-09-20 18:23:43 -0400

    expand the navigation pane to show text next to icons

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import Logo from "./Logo";
 import NavigationLinks from "./NavigationLinks";
-import { ArrowCircleRightIcon } from "@heroicons/react/outline";
+import {
+  ArrowCircleRightIcon,
+  ArrowCircleLeftIcon,
+} from "@heroicons/react/outline";
 
 function SideBar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="flex justify-around px-3 w-screen fixed bottom-0  md:fixed md:top-0 md:w-auto md:flex-col  py-6 md:shadow-2xl">
+    <div
+      className={`flex md:flex-col md:w-auto md:flex ${
+        isExpanded ? "justify-around" : "justify-start"
+      } px-3 w-screen fixed bottom-0  md:fixed md:top-0 py-6 md:shadow-2xl`}
+    >
       <Logo />
-      <NavigationLinks />
-      <button aria-label="Expand sidebar">
-        <ArrowCircleRightIcon className=" hidden md:block md:w-12 md:h-12 md:text-gray-500 nav-link" />
+      <NavigationLinks isExpanded={isExpanded} />
+      <button aria-label="Expand sidebar" onClick={toggleExpand}>
+        <ArrowCircleRightIcon
+          className={`md:w-12 md:h-12 md:text-gray-500 nav-link ${
+            isExpanded ? "hidden" : "block"
+          }`}
+        />
       </button>
+      {isExpanded && (
+        <button onClick={() => toggleExpand(false)} className="ml-5">
+          <ArrowCircleLeftIcon
+            className={`md:w-12 md:h-12 md:text-gray-500 nav-link ${
+              isExpanded ? "block" : "hidden"
+            }`}
+          />
+        </button>
+      )}
     </div>
   );
 }
